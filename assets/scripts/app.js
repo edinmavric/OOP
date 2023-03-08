3affad93ea0ae482eee50d52f8f98dea0ceea27a
edinmavric <edinmavric10@gmail.com>
2023-03-08 19:51:45 +0100

    instanceof

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -65,7 +65,7 @@ class ShoppingCart extends Component {
   constructor(renderHookId) {
     super(renderHookId, false);
     this.orderProducts = () => {
-      console.log('Ordering...');
+      console.log("Ordering...");
       console.log(this.items);
     };
     this.render();
@@ -78,15 +78,15 @@ class ShoppingCart extends Component {
   }
 
   render() {
-    const cartEl = this.createRootElement('section', 'cart');
+    const cartEl = this.createRootElement("section", "cart");
     cartEl.innerHTML = `
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
-    const orderButton = cartEl.querySelector('button');
+    const orderButton = cartEl.querySelector("button");
     // orderButton.addEventListener('click', () => this.orderProducts());
-    orderButton.addEventListener('click', this.orderProducts);
-    this.totalOutput = cartEl.querySelector('h2');
+    orderButton.addEventListener("click", this.orderProducts);
+    this.totalOutput = cartEl.querySelector("h2");
   }
 }
 
@@ -102,7 +102,7 @@ class ProductItem extends Component {
   }
 
   render() {
-    const prodEl = this.createRootElement('li', 'product-item');
+    const prodEl = this.createRootElement("li", "product-item");
     prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -114,8 +114,8 @@ class ProductItem extends Component {
           </div>
         </div>
       `;
-    const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
   }
 }
 
@@ -131,30 +131,30 @@ class ProductList extends Component {
   fetchProducts() {
     this.#products = [
       new Product(
-        'A Pillow',
-        'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-        'A soft pillow!',
+        "A Pillow",
+        "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+        "A soft pillow!",
         19.99
       ),
       new Product(
-        'A Carpet',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-        'A carpet which you might like - or not.',
+        "A Carpet",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+        "A carpet which you might like - or not.",
         89.99
-      )
+      ),
     ];
     this.renderProducts();
   }
 
   renderProducts() {
     for (const prod of this.#products) {
-      new ProductItem(prod, 'prod-list');
+      new ProductItem(prod, "prod-list");
     }
   }
 
   render() {
-    this.createRootElement('ul', 'product-list', [
-      new ElementAttribute('id', 'prod-list')
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
     ]);
     if (this.#products && this.#products.length > 0) {
       this.renderProducts();
@@ -168,8 +168,8 @@ class Shop {
   }
 
   render() {
-    this.cart = new ShoppingCart('app');
-    new ProductList('app');
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
   }
 }
 
@@ -187,3 +187,5 @@ class App {
 }
 
 App.init();
+
+// title instanceof Component;
